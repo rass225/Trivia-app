78d91d5cc3da576699921b0a23e8ff7a9c78d471
rass225 <31581061+rass225@users.noreply.github.com>
2018-04-22 14:14:36 -0500

    Add files via upload

---
+++ Pseudocode.java
@@ -0,0 +1,9 @@
+/* My pseudocode
+ * 1. Receive question and answer files
+ * 2. Import the data from them.
+ * 3. Check if the Questions are the same
+ * 4. Check if answers are correct
+ * 5. Collect the answers 
+ * 6. Produce PowerPoint with questions and with the answers given by the participants 
+ * 
+ */
